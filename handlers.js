1a7f228a9a3fc6e890d7f8395bc0a7d0c4fe82b4
narayang <narayang@google.com>
2013-12-06 11:47:44 -0800

    first checkin for node app

---
+++ handlers.js
@@ -0,0 +1,45 @@
+
+var exec = require("child_process").exec;var querystring = require("querystring");
+
+function start(response, request) {
+  console.log("handler for start called");
+
+var body = '<html>'+
+ '<head>'+
+ '<meta http-equiv="Content-Type" content="text/html; '+
+ 'charset=UTF-8" />'+
+ '</head>'+
+ '<body>'+
+ '<form action="/upload" method="post">'+
+ '<textarea name="name"></textarea>'+
+ '<input type="submit" value="Submit text" />'+
+ '</form>'+
+ '</body>'+
+ '</html>'; response.writeHead(200, {"Content-Type": "text/html"});
+ response.write(body);
+ response.end();
+
+}
+
+function upload(response, request) {
+  
+  var postData = "";
+  console.log("handler for upload called");
+
+   request.addListener("data", function(postDataChunk) {
+ 	console.log("Receiving POST data chunk:"+ postDataChunk.toString());
+	postData += postDataChunk.toString();
+    });    request.addListener("end", function() {
+ 	console.log("Done receiving the data");
+  	response.writeHead(200, {"Content-Type": "text/plain"});
+  	response.write("hare krsna: " + querystring.parse(postData).name);
+  	response.end();
+    });
+
+
+}
+
+exports.start = start;
+exports.upload = upload;
+
+
---
+++ server.js
@@ -0,0 +1,20 @@
+
+
+var http = require("http");
+var url = require("url");
+var formidable = require("formidable");
+
+function start(route, handle) {
+  function onRequest(request, response) {
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for " + pathname + " received.");
+
+    request.setEncoding("utf8");
+    route(handle, pathname, response, request);
+  }
+
+  http.createServer(onRequest).listen(8888);
+  console.log("Server has started.");
+}
+
+exports.start = start;
