8216edf8b657e58fc13ef91246b60c3303ccafeb
narayang <narayang@google.com>
2013-12-12 19:41:55 -0800

    adding azure publish file

---
+++ app.js
@@ -0,0 +1,12 @@
+var server = require('index');
+var router = require("router");
+var requestHandlers = require("handlers");
+
+var handle = {}
+
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+handle["/show"] = requestHandlers.show;
+
+server.start(router.route, handle);
